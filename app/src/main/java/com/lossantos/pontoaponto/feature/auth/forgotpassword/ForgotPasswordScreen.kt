a8fd3fae6ca9f67536a0feb0266d16beb37ad289
Samuel Marques <samuel.s.marques@protonmail.com>
2024-04-30 14:30:57 -0300

    Create ForgotPasswordScreen.kt

---
+++ app/src/main/java/com/lossantos/pontoaponto/feature/auth/forgotpassword/ForgotPasswordScreen.kt
@@ -0,0 +1,105 @@
+package com.lossantos.pontoaponto.feature.auth.forgotpassword
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.lossantos.pontoaponto.feature.auth.components.AuthComponents
+
+class ForgotPasswordScreen(private val navController: NavController? = null) {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Screen() {
+        Scaffold(topBar = {
+            TopAppBar(title = { /*TODO*/ }, navigationIcon = {
+                if (navController?.previousBackStackEntry != null) {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Voltar")
+                    }
+                }
+            }, modifier = Modifier.padding(0.dp))
+        }) { it ->
+            Column(
+                verticalArrangement = Arrangement.spacedBy(
+                    10.dp,
+                    alignment = Alignment.CenterVertically
+                ), horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(it)
+                    .padding(horizontal = 24.dp, vertical = 30.dp)
+                    .fillMaxWidth()
+            ) {
+                AuthComponents().Header(
+                    title = "Esqueceu a senha?",
+                    subtitle = "Por favor, insira seu e-mail ou seu CPF."
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Fields()
+                Spacer(modifier = Modifier.weight(1f))
+                ForgotPasswordButton()
+            }
+        }
+    }
+
+    @Composable
+    fun Fields(modifier: Modifier = Modifier) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            AuthComponents().EmailField(allowCPF = true)
+        }
+    }
+
+    @Composable
+    fun ForgotPasswordButton(modifier: Modifier = Modifier) {
+        Button(
+            onClick = {
+                // todo: implementar esqueci a senha
+            },
+            contentPadding = PaddingValues(10.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(color = 0xFF3755C1)),
+            shape = RoundedCornerShape(12.dp),
+            modifier = modifier
+                .heightIn(min = 44.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Recuperar senha",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.labelSmall.copy(fontWeight = FontWeight.Bold),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ForgotPasswordScreenPreview() {
+    ForgotPasswordScreen().Screen()
+}
