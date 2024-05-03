f616f92764f80e6d527f6ca7b7b164b2de390964
Samuel Marques <samuel.s.marques@protonmail.com>
2024-05-03 14:31:32 -0300

    Update MainActivity.kt

--- app/src/main/java/com/lossantos/pontoaponto/MainActivity.kt
+++ app/src/main/java/com/lossantos/pontoaponto/MainActivity.kt
@@ -16,6 +16,10 @@ import androidx.navigation.compose.rememberNavController
 import com.lossantos.pontoaponto.feature.auth.login.LoginScreen
 import com.lossantos.pontoaponto.feature.auth.login.LoginWithEmailAndPasswordScreen
 import com.lossantos.pontoaponto.feature.auth.forgotpassword.ForgotPasswordScreen
+import com.lossantos.pontoaponto.feature.auth.signup.SignupConfirmCodeScreen
+import com.lossantos.pontoaponto.feature.auth.signup.SignupPersonalDataScreen
+import com.lossantos.pontoaponto.feature.auth.signup.SignupScreen
+import com.lossantos.pontoaponto.feature.home.HomeScreen
 import com.lossantos.pontoaponto.ui.theme.PontoAPontoTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +42,10 @@ class MainActivity : ComponentActivity() {
                 LoginWithEmailAndPasswordScreen(navController = navController).Screen()
             }
             composable("forgot_password") { ForgotPasswordScreen(navController = navController).Screen() }
+            composable("signup") { SignupScreen(navController = navController).Screen() }
+            composable("signup_personal_data") { SignupPersonalDataScreen(navController = navController).Screen() }
+            composable("signup_confirm_code") { SignupConfirmCodeScreen(navController = navController).Screen() }
+            composable("home") { HomeScreen(navController = navController).Screen() }
         }
     }
 }
