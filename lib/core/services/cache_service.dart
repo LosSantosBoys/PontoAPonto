4e673d5e664f3925de86568ebe4c4b0915f06b0c
Samuel Marques <samuel.s.marques@protonmail.com>
2024-07-30 17:55:45 -0300

    renomeia boxes

--- lib/core/services/cache_service.dart
+++ lib/core/services/cache_service.dart
@@ -2,22 +2,22 @@ import 'package:hive/hive.dart';
 
 class CacheService {
   Future<void> saveData(String key, dynamic data) async {
-    final Box<dynamic> box = Hive.box('llm_hub');
+    final Box<dynamic> box = Hive.box('pontoaponto');
     await box.put(key, data);
   }
 
   Future<dynamic> getData(String key) async {
-    final Box<dynamic> box = Hive.box('llm_hub');
+    final Box<dynamic> box = Hive.box('pontoaponto');
     return box.get(key);
   }
 
   Future<void> deleteData(String key) async {
-    final Box<dynamic> box = Hive.box('llm_hub');
+    final Box<dynamic> box = Hive.box('pontoaponto');
     await box.delete(key);
   }
 
   Future<void> clearData() async {
-    final Box<dynamic> box = Hive.box('llm_hub');
+    final Box<dynamic> box = Hive.box('pontoaponto');
     await box.clear();
   }
 }
