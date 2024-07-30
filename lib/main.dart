0a1f76b1b921e1b03b26548f3e48f52f9590f23a
Samuel Marques <samuel.s.marques@protonmail.com>
2024-07-30 18:36:38 -0300

    adiciona rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:pontoaponto/core/services/auth_service.dart';
+import 'package:pontoaponto/features/auth/pages/forgot_password_page.dart';
 import 'package:pontoaponto/features/auth/pages/login_page.dart';
+import 'package:pontoaponto/features/auth/pages/sign_up_page.dart';
+import 'package:pontoaponto/features/home/pages/home_page.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 Future<void> main() async {
@@ -20,7 +24,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(),
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginPage(),
+        '/': (context) => FutureBuilder<bool>(
+              future: AuthService().isAuthenticated(),
+              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    return snapshot.data! ? const HomePage() : const LoginPage();
+                  default:
+                    return const Scaffold(
+                      body: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                }
+              },
+            ),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/home': (context) => const HomePage(),
+        '/forgot-password': (context) => ForgotPasswordPage(),
       },
     );
   }
