9786666626b47007bd9f971b1d2d9ea749c760fe
Samuel Marques <samuel.s.marques@protonmail.com>
2024-07-30 17:27:06 -0300

    adiciona dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pontoaponto/features/auth/pages/login_page.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   timeago.setLocaleMessages('pt_BR_short', timeago.PtBrShortMessages());
+  await dotenv.load(fileName: '.env');
 
   runApp(const MyApp());
 }
