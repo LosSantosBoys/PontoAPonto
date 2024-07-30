203e715be279b60a655cdb2dc72722e0e2640245
Samuel Marques <samuel.s.marques@protonmail.com>
2024-07-30 16:34:10 -0300

    adiciona botões e textos

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:pontoaponto/core/widgets/custom_button.dart';
+import 'package:pontoaponto/features/auth/enum/social_enum.dart';
+import 'package:pontoaponto/features/auth/widgets/social_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(24),
+        padding: const EdgeInsets.all(24),
         child: Column(
-          Spacer(),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            // TODO: Adicionar a logo do app
+            Container(
+              width: 210,
+              height: 210,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey[200],
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              "PontoAPonto",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Vamos entrar na sua conta!',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                const SocialButton(type: SocialEnum.google, enabled: false),
+                const SizedBox(height: 10),
+                const SocialButton(type: SocialEnum.meta, enabled: false),
+                const SizedBox(height: 10),
+                CustomButton(text: "Login", onPressed: () {}),
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/signup");
+              },
+              child: const Text(
+                'Não tem conta? Crie uma.',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
