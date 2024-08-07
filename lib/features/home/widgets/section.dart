afaa9262fccc93f6ea03cd8db0453c6a80a32782
Samuel Marques <samuel.s.marques@protonmail.com>
2024-08-07 14:00:02 -0300

    adiciona `titleWidget`

--- lib/features/home/widgets/section.dart
+++ lib/features/home/widgets/section.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class Section extends StatelessWidget {
   const Section({
     super.key,
-    required this.title,
+    this.title,
     required this.child,
+    this.titleWidget,
     this.action,
     this.padding = const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
   });
 
-  final String title;
+  final String? title;
+  final Widget? titleWidget;
   final Widget? action;
   final Widget child;
   final EdgeInsets padding;
@@ -21,22 +23,25 @@ class Section extends StatelessWidget {
       padding: padding,
       child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+          if (titleWidget != null)
+            titleWidget!
+          else
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title ?? "",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                if (action != null) action!,
-              ],
+                  if (action != null) action!,
+                ],
+              ),
             ),
-          ),
           const SizedBox(height: 5),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10),
