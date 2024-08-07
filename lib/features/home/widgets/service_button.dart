c8f0c82cb254261e54ae29e0656f544b8499daac
Samuel Marques <samuel.s.marques@protonmail.com>
2024-08-07 12:34:29 -0300

    corrige ripple n aparecer

--- lib/features/home/widgets/service_button.dart
+++ lib/features/home/widgets/service_button.dart
@@ -13,78 +13,81 @@ class ServiceButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: enabled ? () {} : null,
-      borderRadius: BorderRadius.circular(10),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Builder(builder: (context) {
-              IconData iconData;
-
-              switch (service) {
-                case ServiceEnum.deliveries:
-                  iconData = Icons.delivery_dining;
-                  break;
-                case ServiceEnum.flights:
-                  iconData = Icons.airplanemode_active;
-                  break;
-                case ServiceEnum.offers:
-                  iconData = Icons.local_offer;
-                  break;
-                case ServiceEnum.public:
-                  iconData = Icons.directions_bus;
-                  break;
-                case ServiceEnum.rides:
-                  iconData = Icons.directions_car;
-                  break;
-                case ServiceEnum.rentals:
-                default:
-                  iconData = Icons.car_rental;
-              }
-
-              return Icon(
-                iconData,
-                size: 36,
-                color: enabled ? const Color(0xFF3755C1) : const Color(0xFFD4D6DD),
-              );
-            }),
-            const SizedBox(height: 10),
-            Builder(builder: (context) {
-              String text;
-
-              switch (service) {
-                case ServiceEnum.deliveries:
-                  text = "Entregas";
-                  break;
-                case ServiceEnum.flights:
-                  text = "Voos";
-                  break;
-                case ServiceEnum.offers:
-                  text = "Ofertas";
-                  break;
-                case ServiceEnum.public:
-                  text = "Transport Público";
-                  break;
-                case ServiceEnum.rides:
-                  text = "Corridas";
-                  break;
-                case ServiceEnum.rentals:
-                default:
-                  text = "Aluguel";
-              }
-
-              return Text(
-                text,
-                style: TextStyle(
-                  fontSize: 14,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: enabled ? () {} : null,
+        borderRadius: BorderRadius.circular(10),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Builder(builder: (context) {
+                IconData iconData;
+      
+                switch (service) {
+                  case ServiceEnum.deliveries:
+                    iconData = Icons.delivery_dining;
+                    break;
+                  case ServiceEnum.flights:
+                    iconData = Icons.airplanemode_active;
+                    break;
+                  case ServiceEnum.offers:
+                    iconData = Icons.local_offer;
+                    break;
+                  case ServiceEnum.public:
+                    iconData = Icons.directions_bus;
+                    break;
+                  case ServiceEnum.rides:
+                    iconData = Icons.directions_car;
+                    break;
+                  case ServiceEnum.rentals:
+                  default:
+                    iconData = Icons.car_rental;
+                }
+      
+                return Icon(
+                  iconData,
+                  size: 36,
                   color: enabled ? const Color(0xFF3755C1) : const Color(0xFFD4D6DD),
-                ),
-              );
-            }),
-          ],
+                );
+              }),
+              const SizedBox(height: 10),
+              Builder(builder: (context) {
+                String text;
+      
+                switch (service) {
+                  case ServiceEnum.deliveries:
+                    text = "Entregas";
+                    break;
+                  case ServiceEnum.flights:
+                    text = "Voos";
+                    break;
+                  case ServiceEnum.offers:
+                    text = "Ofertas";
+                    break;
+                  case ServiceEnum.public:
+                    text = "Transport Público";
+                    break;
+                  case ServiceEnum.rides:
+                    text = "Corridas";
+                    break;
+                  case ServiceEnum.rentals:
+                  default:
+                    text = "Aluguel";
+                }
+      
+                return Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: enabled ? const Color(0xFF3755C1) : const Color(0xFFD4D6DD),
+                  ),
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
